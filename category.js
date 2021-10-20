ee2e61c96bfce2c079e2ead808f5e2632e7c5af8
Hari Krishnam Raju <80376013+harikrishnamraju@users.noreply.github.com>
2021-10-20 19:37:25 +0530

    Add files via upload

---
+++ category.js
@@ -0,0 +1,25 @@
+[
+    {
+        "id":1,
+        "category":"action"
+    },
+    {
+        "id":2,
+        "category":"drama"
+    },
+    
+    {
+        "id":3,
+        "category":"comedy"
+    },
+    {
+        "id":4,
+        "category":"thriller"
+    },
+    {
+        "id":5,
+        "category":"music"
+    }
+    
+
+]
---
+++ data.js
@@ -0,0 +1,167 @@
+[
+    {
+        "id": 1,
+        "name":"arjun suravaram",
+        "category_id":1,
+        "Age":"13+",
+        "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/00/64/8bdf39424862a74b0e5673f274bd/1581000285702_premium_510x756_3x4Images.png?impolicy=resize&w=305&h=451.4",
+        "year":2020,
+        "info":"U/A 7+ | Action | Thriller | Enjoy Arjun Suravaram full movie here... Arjun decides to pursue his dream of becoming an investigative journalist but finds himself tangled in the middle of a major corruption scam. He and Kavya set off on an adventure to prove his innocence only to uncover the evil forces waiting to take them down."
+    },
+    {
+        "id": 2,
+        "name":"sahasam",
+        "category_id":1,
+        "Age":"13+",
+        "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/e1/bd/18a7c6f04c97a4aca0e574456025/1579694504084_sahasamhdmovieonline1070x1585_3x4Images.jpg?impolicy=resize&w=333.33333333333337&h=493.33333333333337",
+        "year":2017,
+        "info":"Enjoy sahasam full movie here : An honest police officer finds himself transferred again and again due to his sincerity. After his latest transfer, he comes across a file that involves a gang of ruthless thieves who loot and kill along the highway."
+    },
+    {
+        "id":3,
+        "name":"pournami",
+        "category_id":1,
+        "age":"7+",
+        "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/3e/02/a698d341448da594510960286182/1580383498177_pournamihdmovieonline1070x1585_3x4Images.jpg?impolicy=resize&w=333.33333333333337&h=493.33333333333337",
+        "year":2020,
+        "info":"Sending bright smiles and positive vibes your way, All is well is a talk show hosted by Suma Kanakala, where she interacts with various celebrities and maintains an entertaining tenor while highlighting all the good news from around the world.",
+
+    },
+    {
+        "id":4,
+        "name":"bujjigadu",
+        "category_id":1,
+        "age":"15+",
+        "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/fb/22/58a228e64ae686705fefe8990122/1580295972009_bujjigaduhdmovieonline1070x1585_3x4Images.jpg?impolicy=resize&w=333.33333333333337&h=493.33333333333337",
+        "year":2008,
+        "info":"U/A 13+ | Violence | Exciting | Enjoy Bujjigadu full movie here... Bujji, a Rajinikanth fan, leaves his house after a fight with his childhood friend Chitti. After 12 years, he comes back to meet Chitti but gets caught up in a small brawl. The rest of the story is all about how they meet each other."
+    },
+    
+        {
+            "id": 5,
+            "name":"nippu",
+            "category_id":2,
+            "Age":"13+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/2020/11/7/1604771609438_nippu_1070x1585_3x4Images.png?impolicy=resize&w=333.33333333333337&h=493.33333333333337",
+            "year":2017,
+            "info":"U/A 16+ | Emotional | Heartfelt | Enjoy Vedam full movie here… Five people from diverse walks of life are brought together through bizarre coincidences, find their lives entwined after terrorists wreak havoc at the hospital, and the incidents that follow leave their lives changed forever."
+        },
+        {
+            "id": 6,
+            "name":"Khaki",
+            "category_id":2,
+            "Age":"13+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/2021/07/7/1625679654223_2watchkhakee1920x1080_16x9Images.jpg?impolicy=resize&w=270&h=151.875",
+            "year":2017,
+            "info":"Enjoy Khaki full movie here : An honest police officer finds himself transferred again and again due to his sincerity. After his latest transfer, he comes across a file that involves a gang of ruthless thieves who loot and kill along the highway."
+        },
+        {
+            "id":7,
+            "name":"all is well",
+            "category_id":2,
+            "age":"7+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/2020/10/2/1601645009448_alliswellep8_16x9Images.jpg?impolicy=resize&w=270&h=151.875",
+            "year":2020,
+            "info":"Sending bright smiles and positive vibes your way, All is well is a talk show hosted by Suma Kanakala, where she interacts with various celebrities and maintains an entertaining tenor while highlighting all the good news from around the world.",
+    
+        },
+        {
+            "id":8,
+            "name":"honey moon",
+            "category_id":2,
+            "age":"13+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/2021/02/27/1614452534483_honeymoononline1920x1080_16x9images_16x9Images.jpg?impolicy=resize&w=270&h=151.875",
+            "year":2020,
+            "info":"After consenting to an arranged marriage, Praveen and Tejaswini embark on a journey filled with unexpected events and galore of emotions, and their unplanned honeymoon turns out to be the best decision ever.",
+        },
+        {
+            "id": 9,
+            "name":"bell bottom",
+            "category_id":3,
+            "Age":"13+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/00/64/8bdf39424862a74b0e5673f274bd/1581000285702_premium_510x756_3x4Images.png?impolicy=resize&w=305&h=451.4",
+            "year":2019,
+            "info":"U/A 13+ | Comedy | Crime | Enjoy Bell Bottom Full movie here : Constable Divakar aspires to be a detective and his dream turns into reality when a mysterious robbery occurs at the local police station. During the investigation, he meets several quirky characters that only make it harder to solve the case."
+        },
+        {
+            "id": 10,
+            "name":"galli sampath",
+            "category_id":3,
+            "Age":"13+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/00/64/8bdf39424862a74b0e5673f274bd/1581000285702_premium_510x756_3x4Images.png?impolicy=resize&w=305&h=451.4",
+            "year":2017,
+            "info":"U/A 7+ | Comedy | Drama | Enjoy Gaali Sampath full movie here : Gaali Sampath loses his voice in an accident and unknowingly plays spoilsport in his son's love story. When his son yells at him for sabotaging his plans, a disappointed Sampath leaving home slips into a well left uncovered. How he survives in the well is the crux of the story."
+        },
+        {
+            "id":11,
+            "name":"krack",
+            "category_id":3,
+            "age":"7+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/00/64/8bdf39424862a74b0e5673f274bd/1581000285702_premium_510x756_3x4Images.png?impolicy=resize&w=305&h=451.4",
+            "year":2020,
+            "info":"U/A 13+ | Action | Violence | Enjoy Krack full movie here : When a most-wanted terrorist, a factionist, and a local goon locks horns with an honest cop Potharaju Veerashankar, little do they know that their lives will be changed forever.",
+    
+        },
+        {
+            "id":12,
+            "name":"mail",
+            "category_id":3,
+            "age":"13+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/00/64/8bdf39424862a74b0e5673f274bd/1581000285702_premium_510x756_3x4Images.png?impolicy=resize&w=305&h=451.4",
+            "year":2020,
+            "info":"U | Feel good | Emotional | Enjoy Mail Full movie here : Fascinated by the world of computers and the internet, an innocent 18-year-old Ravi feels elated when a fellow villager Hybath opens a gaming centre. Soon, an email unfurls a series of unexpected events stirring life-altering experience"
+        },
+        {
+            "id": 13,
+            "name":"shylock",
+            "category_id":4,
+            "Age":"13+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/00/64/8bdf39424862a74b0e5673f274bd/1581000285702_premium_510x756_3x4Images.png?impolicy=resize&w=305&h=451.4",
+            "year":2019,
+            "info":"U/A 13+ | Action | Drama | A ruthless moneylender must tackle a wicked man's revenge plan and achieve what's rightfully his."
+        },
+        {
+            "id": 14,
+            "name":"Khadi",
+            "category_id":4,
+            "Age":"13+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/00/64/8bdf39424862a74b0e5673f274bd/1581000285702_premium_510x756_3x4Images.png?impolicy=resize&w=305&h=451.4",
+            "year":2017,
+            "info":"U/A 13+ | Action | Violence | Enjoy Khaidi full movie here... An ex-felon is finally out of prison to meet his daughter, but a twist in the circumstances leaves him in charge of saving unconscious police officers. He, along with Inspector Bejoy, must fight their way through betrayal to defeat the drug lord and emerge victoriously."
+        },
+        {
+            "id":15,
+            "name":"nandi",
+            "category_id":4,
+            "age":"7+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/00/64/8bdf39424862a74b0e5673f274bd/1581000285702_premium_510x756_3x4Images.png?impolicy=resize&w=305&h=451.4",
+            "year":2020,
+            "info":"U/A 16+ | Drama | Violence | Enjoy Naandhi full movie here : Surya Prakash's life takes an unexpected turn when he is wrongly implicated in a murder case and serves prison as an undertrial for five years. When junior lawyer Aadhya enters his life as a ray of hope, the duo wages a legal battle.",
+    
+        },
+        {
+            "id":16,
+            "name":"color photo",
+            "category_id":4,
+            "age":"13+",
+            "image":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/00/64/8bdf39424862a74b0e5673f274bd/1581000285702_premium_510x756_3x4Images.png?impolicy=resize&w=305&h=451.4",
+            "year":2020,
+            "info":"  U/A 7+ | Romance | Emotional | Enjoy Colour Photo Full movie here : A love story between Jaya Krishna and Deepti, set in the late 1990s, comes alive in this periodic drama.",
+        },
+        {
+            "id": 17,
+            "album":"beemla nayak",
+            "category_id":5, 
+            "formate":"mp3",
+            "year":2019,
+           
+        },
+        {
+            "id": 18,
+            "album":"bahubali",
+            "category_id":5,
+            "formate":"mp3",  
+            "year":2017,
+        
+        }
+]
